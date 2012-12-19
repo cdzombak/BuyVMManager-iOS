8a8da6cbb9e908293dd0bc579e1fdb151340d89c
Chris Dzombak <chris@chrisdzombak.net>
2012-12-19 12:53:07 -0500

    Initial commit of empty xcode project with AFNetworking pod

---
+++ buyvmmanager/BVMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BVMAppDelegate.h
+//  buyvmmanager
+//
+//  Created by Chris Dzombak on 12/19/12.
+//  Copyright (c) 2012 Chris Dzombak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BVMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ buyvmmanager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  buyvmmanager
+//
+//  Created by Chris Dzombak on 12/19/12.
+//  Copyright (c) 2012 Chris Dzombak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BVMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BVMAppDelegate class]));
+    }
+}
